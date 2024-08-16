44b4041c011aa16a666d401cec8b0a82eda1eedc
softcoffee1 <yunseongho99@gmail.com>
2024-08-16 21:46:33 +0900

    Added Cross Attention implementation from scratch, Not changing Embed_CrossAttention class

--- train.py
+++ train.py
@@ -7,24 +7,23 @@ from dataloader import load_data
 
 features, labels = load_data()
 
+print("진행중..")
+
 batch_size = 32
 learning_rate = 0.001
 num_epochs = 50
 
 dataset = TensorDataset(features, labels)
-train_size = int(0.9 * len(dataset)) 
+train_size = int(0.9 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-model = Baseline_model(in_channel=5,
-                       out_channel=64,
-                       input_dim=62,
-                       final_dim=3,
-                       num_heads=8,
-                       topk=25)
+model = Baseline_model(
+    in_channel=5, out_channel=64, input_dim=62, final_dim=3, num_heads=8, topk=25
+)
 
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -59,7 +58,7 @@ def evaluate(model, test_loader, criterion):
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    
+
     accuracy = correct / total
     avg_loss = test_loss / len(test_loader)
     print(f"Test Loss: {avg_loss:.4f}, Test Accuracy: {accuracy:.4f}")
@@ -68,7 +67,7 @@ def evaluate(model, test_loader, criterion):
 # 학습 및 테스트 루프
 for epoch in range(num_epochs):
     train(model, train_loader, criterion, optimizer, epoch)
-    
+
     # 5 에포크마다 테스트
     if (epoch + 1) % 2 == 0:
         print(f"\nTesting at epoch {epoch + 1}:")
