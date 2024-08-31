b1a27693733c152839034ee2e7e8c7c69d9a1929
SungHo Yoon <65325476+SoftCoffee1@users.noreply.github.com>
2024-08-31 23:40:18 +0900

    멘토님이 주신 dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,54 +1,61 @@
-import numpy as np
+## 모든 데이터에 적용되는 파트는 모델이 아닌 data_loader측에서 적용하는게 좋습니다. 
+## 아래와 같이 position embedding을 따로 넣어주는 코드도 포함시켰습니다.
+import random
 import torch
+import torch.nn as nn
+import numpy as np
 
+from torch.utils.data import Dataset
 
-def load_data(test_no, masking_mode, masking_ratio, random_seed):
-    np.random.seed(random_seed)
+def sinusoid_encoding_table(n_seq, d_hidn):
+    
+    def cal_angle(position, i_hidn):
+        return position / np.power(10000, 2 * (i_hidn // 2) / d_hidn)
+    
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, i_hidn) for i_hidn in range(d_hidn)]
 
-    train_data_list = []
-    train_label_list = []
-    test_data_list = []
-    test_label_list = []
+    sinusoid_table = np.array([get_posi_angle_vec(i_seq) for i_seq in range(n_seq)])
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # even index (sin) 
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # odd index (cos)
+    return sinusoid_table
 
-    for i in range(15):
-        dataname = f'./SEED_data/subject_{i}data.npy'
-        labelname = f'./SEED_data/subject_{i}label.npy'
-        data = np.load(dataname)
-        label = np.load(labelname)
+class SeedDataset(Dataset):
+    def __init__(self, dataset, training=True):    
+        self.frames = 265   # 변경           
+        self.dim_embed = 64         
+        self.channels = 62
+        self.training = training  
+        
+        self.dataset = torch.load(dataset,weights_only=True)  
+        self.frame_encoding_table = torch.FloatTensor(sinusoid_encoding_table(self.frames + 1, self.dim_embed))
+        self.frame_encodding = nn.Embedding.from_pretrained(self.frame_encoding_table, freeze=True)
 
-        if i == test_no:
-            test_data_list.append(data)
-            test_label_list.append(label)
-        else:
-            if masking_mode:
-                mask1 = np.random.rand(62) < masking_ratio
-                mask2 = np.random.rand(265) < masking_ratio
-                masked_data = np.copy(data)
-                masked_data[:, mask1, :, :] = 0
-                masked_data[:, :, mask2, :] = 0
-                
-                train_data_list.append(masked_data)
-            else:
-                train_data_list.append(data)
+        
+    def __len__(self):
+        return len(self.dataset["input"])
+    
+    def __getitem__(self, idx):
+        inputs = self.dataset["input"][idx, ...]
+        labels = self.dataset["label"][idx, ...]
+        masks = self.dataset["mask"][idx, ...]
+        
+		    
+        if self.training:
+            T_max = masks.sum()
+            T = random.randint(0, int(0.5 * T_max))
+            N = random.randint(0, int(0.5 * self.channels))
+            t1 = random.randint(0, T_max - T)
+            t2 = t1 + T
+            n1 = random.randint(0, self.channels - N)
+            n2 = n1 + N
+            inputs[t1:t2, n1:n2, :] = 0.0
             
-            train_label_list.append(label)
-
-    concatenated_data = np.concatenate(train_data_list, axis=0)
-    concatenated_label = np.concatenate(train_label_list, axis=0)
-
-    sample_feature = torch.tensor(concatenated_data, dtype=torch.float32)
-
-    labels_one_hot = torch.zeros(concatenated_label.shape[0], 3)
-    index = torch.tensor(concatenated_label + 1, dtype=torch.int64)
-    labels_one_hot.scatter_(1, index, 1.0)
-
-    test_concatenated_data = np.concatenate(test_data_list, axis=0)
-    test_concatenated_label = np.concatenate(test_label_list, axis=0)
-
-    test_sample_feature = torch.tensor(test_concatenated_data, dtype=torch.float32)
-
-    test_labels_one_hot = torch.zeros(test_concatenated_label.shape[0], 3)
-    test_index = torch.tensor(test_concatenated_label + 1, dtype=torch.int64)
-    test_labels_one_hot.scatter_(1, test_index, 1.0)
-
-    return sample_feature, labels_one_hot, test_sample_feature, test_labels_one_hot
+        frame_pos_masks = masks.eq(0)
+        frame_pos = torch.arange(inputs.size(0)) + 1
+        frame_pos = frame_pos.masked_fill(frame_pos_masks, 0)
+        frame_pos_embeddings = self.frame_encodding(frame_pos)
+        frame_pos_embeddings = frame_pos_embeddings.reshape(self.frames, 1, self.dim_embed)
+        frame_pos_embeddings = frame_pos_embeddings.expand(self.frames, self.channels, self.dim_embed)
+        
+        return inputs, frame_pos_embeddings, labels
